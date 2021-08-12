625c5c6872d67da12e8d4879cde97f833f9ced25
christina <chrstnaheadley@gmail.com>
2021-08-12 18:35:26 -0400

    completes user_input exercises

--- ruby_basics/user_input.rb
+++ ruby_basics/user_input.rb
@@ -178,26 +178,60 @@
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
-num1 = nil
-num2 = nil
-loop do
-  puts ">> Please enter a positive or negative integer:"
-  num1 = gets.chomp
-  if valid_number?(num1)
-    num1 = num1.to_i
-  else
-    puts "Invalid input. Only non-zero integers are allowed."
-    
-  end
-  puts ">> Please enter a positive or negative integer:"
-  num2 = gets.chomp
-  if valid_number?(num2)
-    num2 = num2.to_i
-  else
-    puts "Invalid input. Only non-zero integers are allowed."
-  end
+# num1 = nil
+# num2 = nil
+# loop do
+#   loop do
+#     puts ">> Please enter a positive or negative integer:"
+#     num1 = gets.chomp
+#     if valid_number?(num1)
+#       num1 = num1.to_i
+#       break
+#     else
+#       puts "Invalid input. Only non-zero integers are allowed."
+#     end
+#   end
+#   loop do
+#     puts ">> Please enter a positive or negative integer:"
+#     num2 = gets.chomp
+#     if valid_number?(num2)
+#       num2 = num2.to_i
+#       break
+#     else
+#       puts "Invalid input. Only non-zero integers are allowed."
+#     end
+#   end
+  
+#   if num1 < 0 && num2 < 0 || num1 > 0 && num2 > 0
+#     puts "Sorry. One integer must be positive, one must be negative.
+#     >> Please start over."
+#   else
+#     puts "#{num1} + #{num2} = #{num1 + num2}"
+#     break
+#   end
+# end
 
+# A:
+# def read_number
+#   loop do
+#     puts '>> Please enter a positive or negative integer:'
+#     number = gets.chomp
+#     return number.to_i if valid_number?(number)
+#     puts '>> Invalid input. Only non-zero integers are allowed.'
+#   end
+# end
 
-end
+# first_number = nil
+# second_number = nil
 
-# opposite
+# loop do
+#   first_number = read_number
+#   second_number = read_number
+#   break if first_number * second_number < 0
+#   puts '>> Sorry. One integer must be positive, one must be negative.'
+#   puts '>> Please start over.'
+# end
+
+# sum = first_number + second_number
+# puts "#{first_number} + #{second_number} = #{sum}"
+
